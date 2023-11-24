1f56cfd5450a05ff9a3a76a48f3a41870ae8a107
Julián Hinojosa Gil <julianhinojosagil@Julians-MacBook-Air.local>
2023-11-24 12:20:06 +0100

    very small update

--- src/main.c
+++ src/main.c
@@ -1,12 +1,14 @@
+#include "stb_image.h"
 #include "tigr.h"
 #include <stdio.h>
+
+#define STB_IMAGE_IMPLEMENTATION
 
 int main(int argc, char *argv[]) {
   Tigr *screen = tigrWindow(320, 240, "Hello", 0);
 
   while (!tigrClosed(screen)) {
-    tigrLoadImage("../images.png");
-    // tigrClear(screen, tigrRGB(0x80, 0x90, 0xa0));
+    tigrClear(screen, tigrRGB(0x80, 0x90, 0xa0));
     tigrPrint(screen, tfont, 120, 110, tigrRGB(0xff, 0xff, 0xff),
               "Hello, world.");
     tigrUpdate(screen);
