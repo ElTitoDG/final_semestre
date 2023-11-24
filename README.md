4e78c770c416ec83d37b452e302d7e8d8011f1a8
Julián Hinojosa Gil <julianhinojosagil@Julians-MacBook-Air.local>
2023-11-24 11:39:07 +0100

    test image

---
+++ README.md
@@ -0,0 +1,5 @@
+# Proyecto final semestre
+
+Test
+
+Tesr
--- src/main.c
+++ src/main.c
@@ -5,7 +5,8 @@ int main(int argc, char *argv[]) {
   Tigr *screen = tigrWindow(320, 240, "Hello", 0);
 
   while (!tigrClosed(screen)) {
-    tigrClear(screen, tigrRGB(0x80, 0x90, 0xa0));
+    tigrLoadImage("../images.png");
+    // tigrClear(screen, tigrRGB(0x80, 0x90, 0xa0));
     tigrPrint(screen, tfont, 120, 110, tigrRGB(0xff, 0xff, 0xff),
               "Hello, world.");
     tigrUpdate(screen);
