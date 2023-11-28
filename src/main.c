d3d78832eee7a5f35831caaa065cdc75e156d91c
Julián Hinojosa Gil <julianhinojosagil@Julians-MacBook-Air.local>
2023-11-28 13:57:09 +0100

    added backgroud

--- src/main.c
+++ src/main.c
@@ -1,11 +1,28 @@
 #include "tigr.h"
 
 int main(int argc, char *argv[]) {
-  Tigr *screen = tigrWindow(320, 240, "Hello", 0);
-  while (!tigrClosed(screen)) {
-    tigrClear(screen, tigrRGB(0x80, 0x90, 0xa0));
-    tigrPrint(screen, tfont, 120, 110, tigrRGB(0xff, 0xff, 0xff),
-              "Hello, world.");
+
+  // Base Variable
+
+  Tigr *screen, *background, *player;
+
+  screen = tigrWindow(320, 240, "Game 1", 0);
+  background = tigrBitmap(screen->w, screen->h);
+
+  // Main background
+  tigrClear(background, tigrRGB(80, 180, 255));
+  // Green grass
+  tigrFill(background, 0, 200, 320, 40, tigrRGB(60, 120, 60));
+  // Black border
+  tigrFill(background, 0, 200, 320, 3, tigrRGB(0, 0, 0));
+  // White line
+  tigrLine(background, 0, 201, 320, 201, tigrRGB(255, 255, 255));
+
+  // Main Loop
+  while (!tigrClosed(screen) && !tigrKeyDown(screen, TK_ESCAPE)) {
+    // compose the background
+    tigrBlit(screen, background, 0, 0, 0, 0, background->w, background->h);
+    // Update screen input
     tigrUpdate(screen);
   }
   tigrFree(screen);
