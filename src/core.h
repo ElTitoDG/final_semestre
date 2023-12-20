491dcd82275da175d8202f1c43dd838ce44b9f8d
ElTitoDG <43906735+ElTitoDG@users.noreply.github.com>
2023-12-20 11:41:30 +0100

    Revert "Shooting mechanic"

--- src/core.h
+++ src/core.h
@@ -1,18 +1,8 @@
-#ifdef CORE_H
-#define CORE_H
+#include "tigr.h"
+#ifdef __CORE_H__
+#define __CORE_H__
 
-typedef struct Tplayer {
-  float x, y, speed;
-} TPlayer;
+void update(float *dt, float *remaining, Tigr *screen, float *playerx,
+            float *playery, float *playerxs, float *playerys);
 
-typedef struct {
-  float x, y, speed;
-  int active;
-} TProjectile;
-
-// void update(float *dt, float *remaining, Tigr *screen, float *playerx,
-//             float *playery, float *playerxs, float *playerys);
-
-void updatePlayer(TPlayer *player, Tigr *screen);
-void drawPlayer(TPlayer *player, Tigr *screen);
-#endif // CORE_H
+#endif // DEBUG
