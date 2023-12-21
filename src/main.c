1f8bdcebb6a62751db77006487d722dc5794475a
ElTitoDG <julianhinojosagil@gmail.com>
2023-12-21 11:57:30 +0100

    instrucciones en pantalla

--- src/main.c
+++ src/main.c
@@ -49,6 +49,10 @@ int main(int argc, char *argv[]) {
   screen = tigrWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Game 1", 0);
   background = tigrBitmap(screen->w, screen->h);
 
+  char *text = (char *)tigrReadFile("res/info.txt", 0);
+  if (!text)
+    tigrError(0, "Cannot load info.txt");
+
   // Inicialización de las structuras con 
   // sus tipos de datos correspondientes
   TPlayer player = {SCREEN_WIDTH / 2.0f, SCREEN_HEIGHT - PLAYER_HEIGHT - 10,
@@ -66,8 +70,8 @@ int main(int argc, char *argv[]) {
   // Green grass
   tigrFill(background, 0, player.y - 80, SCREEN_WIDTH, 2, tigrRGB(0, 0, 0));
 
-  // // White line
-  tigrLine(background, 0, 201, 320, 201, tigrRGB(255, 255, 255));
+  // White line
+  // tigrLine(background, 0, 201, 320, 201, tigrRGB(255, 255, 255));
 
 
   // Bucle principal de juego
@@ -86,6 +90,8 @@ int main(int argc, char *argv[]) {
 
     drawPlayer(&player, screen);
     drawProjectiles(projectiles, screen);
+    tigrPrint(screen, tfont, 2, 3, tigrRGBA(0xc0, 0xd0, 0xff, 0xc0),
+              text);
    
     // Update screen input
     tigrUpdate(screen);
